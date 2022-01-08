394728480da3fc02ad0afb05e5a4bba7be8dbc1b
kojipy <43063676+kojipy@users.noreply.github.com>
2022-01-08 21:49:52 +0900

    init

---
+++ src/hello_world.cpp
@@ -0,0 +1,21 @@
+//hello.cpp
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+
+// This is One Line Comment
+
+/*
+This is
+mulit line 
+comment
+*/
+
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    cout << floor(log10(10)+1) << endl;
+}
